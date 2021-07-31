f1eb94c842e3aa749f9ebf3e0dc08e731bf4fa6c
jgrindall <john.grindall@gmail.com>
2021-07-31 11:05:55 +0100

    Add more info

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,27 @@ app.listen(port, () => {
  - example folder structure:
     - assets/full
     - assets/thumbs
+
+
+    How can you set up your server and route so that your project remains scalable?
+
+    Create a separate module for your processing functionality
+
+    You are only required to work with jpg files
+
+    At a minimum, you should have at least one test for your endpoint and at least one test for your image processing
+
+    Add caching to your application so that repeated requests to your endpoint use pre-stored images
+
+    Think of edge-cases. Should they get different error messages based on what's wrong?
+
+
+    Does stopping and restarting your server cause any issues?
+    Does your user get feedback when something goes wrong?
+    Is your code easy to follow? Comments?
+
+    Provide all necessary information in your readme file, so your reviewer knows how to test your API.
+
  */
 
 
