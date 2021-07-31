35b301997c8be37d24748b24ac8e7f835a9143e4
jgrindall <john.grindall@gmail.com>
2021-07-31 15:09:51 +0100

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import csv from 'csvtojson';
 import apiRoutes from './routes/api/index';
 
 const app = express();
@@ -7,54 +6,6 @@ const port = 3000;
 
 app.use('/api', apiRoutes);
 
-async function process(file:string){
-    const json = await csv().fromFile(file);
-    json.forEach(item=>{
-        item.phone = item.phone || 'missing data';
-    });
-    console.log(json);
-    return JSON.stringify(json);
-}
-
-app.get('/users', async (req, res)=>{
-    console.log(__dirname);
-    const out = await process(__dirname + '/../users.csv');
-    res.send(out);
-});
-
-// start the Express server
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
-
-
-/**
- - example URL api/images?filename=argentina&width=200&height=200
- - should be usable as an img src
- - example folder structure:
-    - assets/full
-    - assets/thumbs
-
-
-    How can you set up your server and route so that your project remains scalable?
-
-    Create a separate module for your processing functionality
-
-    You are only required to work with jpg files
-
-    At a minimum, you should have at least one test for your endpoint and at least one test for your image processing
-
-    Add caching to your application so that repeated requests to your endpoint use pre-stored images
-
-    Think of edge-cases. Should they get different error messages based on what's wrong?
-
-
-    Does stopping and restarting your server cause any issues?
-    Does your user get feedback when something goes wrong?
-    Is your code easy to follow? Comments?
-
-    Provide all necessary information in your readme file, so your reviewer knows how to test your API.
-
- */
-
-
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,8 +1,41 @@
 import express from 'express';
-import route1 from './route1';
+import logger from '../middleware/logger';
+import validator from '../middleware/validator';
+import {Resizer} from '../../utilities/utils';
+import {validationResult} from "express-validator";
 
-const apiRouter = express.Router();
+export default express.Router()
+    .get('/images2', logger, validator, async (req: express.Request, res: express.Response) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(422)
+                    .send(errors.array().map(e => e.msg).join(","));
+            }
+            const filename: string = req.query.filename as string;
+            const width: string = req.query.width as string;
+            const height: string = req.query.height as string;
 
-apiRouter.use('/route1', route1);
-
-export default apiRouter;
+            const resizer = new Resizer(filename, parseInt(width), parseInt(height));
+            resizer.setInputDir(__dirname + "../../../assets");
+            resizer.setOutputDir(__dirname + "../../../assets/out");
+            try {
+                resizer
+                    .process()
+                    .then(()=>{
+                        return res
+                            .status(200)
+                            .send(filename + ',' + width + ',' + height + ' ' + typeof height);
+                    })
+                    .catch((e:any)=>{
+                        console.log(e);
+                        return res
+                            .status(500)
+                            .send(e);
+                    })
+            }
+            catch (e) {
+                console.log("error resizing");
+            }
+        }
+    );
