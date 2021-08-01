bdd361bd995c01f165691e23c413bc6db8fc49ed
jgrindall <john.grindall@gmail.com>
2021-08-01 20:19:23 +0100

    refactor a bit

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,41 +1,50 @@
 import express from 'express';
 import logger from '../middleware/logger';
 import validator from '../middleware/validator';
-import {Resizer} from '../../utilities/utils';
+import Resizer from '../../utilities/Resizer';
+import resizerConfig from '../../utilities/ResizerConfig';
 import {validationResult} from "express-validator";
 
 export default express.Router()
-    .get('/images2', logger, validator, async (req: express.Request, res: express.Response) => {
+    .get('/images', logger, validator, async (req: express.Request, res: express.Response) => {
+            // get errors from the validation
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res
                     .status(422)
                     .send(errors.array().map(e => e.msg).join(","));
             }
-            const filename: string = req.query.filename as string;
-            const width: string = req.query.width as string;
-            const height: string = req.query.height as string;
 
-            const resizer = new Resizer(filename, parseInt(width), parseInt(height));
-            resizer.setInputDir("../../assets");
-            resizer.setOutputDir("../../assets/out");
+            // input is valid
+            const filename: string = req.query.filename as string;
+            const width: number = parseInt(req.query.width as string);
+            const height: number = parseInt(req.query.height as string);
+
             try {
+                console.log('1');
+                const resizer = new Resizer(resizerConfig.inputPath, resizerConfig.outputPath);
                 resizer
-                    .process()
-                    .then(()=>{
+                    .getResizedImage(filename + ".jpg", width, height)
+                    .then((data) => {
                         return res
+                            .type('image/jpg')
                             .status(200)
-                            .send(filename + ',' + width + ',' + height + ' ' + typeof height);
+                            .end(data);
                     })
-                    .catch((e:any)=>{
-                        console.log(e);
+                    .catch((e: Error) => {
+                        console.log(e, typeof e);
+                        console.log('2');
                         return res
                             .status(500)
-                            .send(e);
+                            .send(e.message);
                     })
             }
             catch (e) {
+                console.log('3');
                 console.log("error resizing");
+                return res
+                    .status(500)
+                    .send(e);
             }
         }
     );
