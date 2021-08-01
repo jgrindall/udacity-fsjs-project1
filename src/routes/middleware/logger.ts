5661e0ea62e943126d849f4630fd162b3d17a896
jgrindall <john.grindall@gmail.com>
2021-08-01 20:19:00 +0100

    correct middleware

--- src/routes/middleware/logger.ts
+++ src/routes/middleware/logger.ts
@@ -1,10 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import type { RequestHandler } from "express";
 
-const logger:RequestHandler = (req:Request, res:Response, next:NextFunction)=>{
+export default function(req:Request, res:Response, next:NextFunction){
     console.log('logger,', req.path, req.url);
     console.log(req.originalUrl);
     next();
 };
-
-export default logger;
--- src/routes/middleware/validator.ts
+++ src/routes/middleware/validator.ts
@@ -1,5 +1,6 @@
 import {query} from "express-validator";
 
+// min and mas sizes for our image
 const validatorOptions = {
     min: 1,
     max: 2048
@@ -11,10 +12,10 @@ const validator = [
         .withMessage("filename cannot be empty"),
     query('width')
         .isInt(validatorOptions)
-        .withMessage("width error"),
+        .withMessage("width error, check your width is a whole number between 1 and 2048"),
     query('height')
         .isInt(validatorOptions)
-        .withMessage("height error"),
+        .withMessage("height error, check your height is a whole number between 1 and 2048"),
 ];
 
 export default validator;
