50e77bf23ca95665647c5a46ca9b107535fea840
jgrindall <john.grindall@gmail.com>
2021-08-02 18:03:36 +0100

    getting eslint working

--- src/eslinttest/index.ts
+++ src/eslinttest/index.ts
@@ -1,14 +1,19 @@
-import {promises as fsPromises} from "fs";
+import fs, {promises as fsPromises} from "fs";
 
-function test() {
-    const a = {
-        f: () => {
-            for(let i = 0; i < 10; i++){
-                console.log(i);
-            }
-        }
-    };
-    a.f();
+function loop(n: number):void{
+    for(let i = 0; i < 10; i++){
+        console.log(i);
+    }
 }
 
-test();
+export async function dirOrFileExists(dir: string): Promise<boolean>{
+    loop(10);
+    return fsPromises
+        .access(dir, fs.constants.F_OK)
+        .then(() => {
+            return Promise.resolve(true);
+        })
+        .catch(() => {
+            return Promise.resolve(false);
+        });
+}
