4b5252c9f1b42f23c37a9fe487a9977c79347723
jgrindall <john.grindall@gmail.com>
2021-08-02 16:29:07 +0100

    -> double quotes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import apiRoutes from './routes/api/index';
+import express from "express";
+import apiRoutes from "./routes/api/index";
 
 const app = express();
 const port = 3000;
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
